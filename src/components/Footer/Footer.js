898e7998161f3fab1a2dc7cc5dfdc29ac5caba6a
Victor <maragiogliov@protonmail.com>
2024-06-22 13:20:06 +0200

    soundcloud on

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,10 +2,20 @@ import React from 'react';
 import './Footer.css';
 
 export default function Footer() {
-  
-
   return (
-    <div className='footer-container'>
+    <div className="footer-container">
+      <iframe
+        width="100%"
+        height="100%"
+        scrolling="yes"
+        frameBorder="no"
+        allow="autoplay"
+        src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1380134788&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=false"
+
+        
+
+      ></iframe>
+ 
     </div>
   );
 }
