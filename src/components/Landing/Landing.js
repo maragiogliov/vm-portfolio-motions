90f68bc382fa05bbfcf6708e5161208691190a99
Victor <maragiogliov@protonmail.com>
2024-06-24 15:35:26 +0200

    go

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -16,7 +16,7 @@ export default function Landing() {
         backgroundColor: gsap.utils.wrap(colors)
       });
 
-      const loop = horizontalLoop(boxes, { paused: false, repeat: -1, speed: 1 });
+      const loop = horizontalLoop(boxes, { paused: false, repeat: -1, speed: 0.8  });
 
       boxes.forEach((boxtest, i) => {
         boxtest.addEventListener("click", () => loop.toIndex(i, { duration: 0.8, ease: "power1.inOut" }));
@@ -38,10 +38,28 @@ export default function Landing() {
         </div>
       </nav>
         <div ref={wrapperRef} className="wrapper">
-          <div ref={el => boxesRef.current[0] = el} className="boxtest">VICTOR</div>
-          <div ref={el => boxesRef.current[1] = el} className="boxtest">MARAGIOGLIO</div>
+
+          <div ref={el => boxesRef.current[0] = el} className="boxtest">Victor</div>
+          <div ref={el => boxesRef.current[1] = el} className="boxtest">Maragioglio</div>
+          <div ref={el => boxesRef.current[2] = el} className="boxtest">—</div>
+          <div ref={el => boxesRef.current[3] = el} className="boxtest">Victor</div>
+          <div ref={el => boxesRef.current[4] = el} className="boxtest">Maragioglio</div>
+          <div ref={el => boxesRef.current[5] = el} className="boxtest">—</div>
     
         </div>
+        <section className="label-text">
+
+        <svg className="flecha-icon" width="20" height="20" viewBox="0 0 52 52" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <rect x="13" y="45" width="38" height="6" fill="black"/>
+          <rect x="51" y="13" width="38" height="6" transform="rotate(90 51 13)" fill="black"/>
+          <rect x="4.67915" y="0.436523" width="65.6669" height="6" transform="rotate(45 4.67915 0.436523)" fill="black"/>
+        </svg>
+
+
+                <div> Freelance </div>
+                <div> Designer & Developer </div>
+
+        </section>
     </section>
   );
 }
