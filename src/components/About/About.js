e116d275340f9dbc75724b834653f3cb4693c58f
maragiogliov <maragiogliov@protonmail.com>
2024-07-16 20:56:41 +0200

    go

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useEffect, useRef} from 'react';
+import gsap from 'gsap';
 import './About.css';
 import victorPic from '../../assets/victorenalbania.jpg';
 import Header from '../Header/Header';
@@ -8,69 +9,83 @@ import Footer from '../Footer/Footer';
 
 
 export default function About() {
+
+  const aboutRef = useRef(null);
+
+
+  // Immediately set opacity to 1 once component mounts
+  useEffect(() => {
+    gsap.set(aboutRef.current, { opacity: 1 });
+  }, []);
+
+
   return (
       <>
-      <Header/>
+      <div className='about-main-container' ref={aboutRef}>
 
-      <div className='container-first'>
+        <Header/>
 
-        <div className='first-title'>Helping brands thrive</div>
-        <div className='first-title'>in the digital world</div>
-        <div className='line'>
-          <div className='circle-overline'></div>
-        </div>
+        <div className='container-first'>
 
-      </div>
-
-      <div className='container-second'>
-
-        <div className='container-textfirst-about'>
-        <div className='container-flechita-text'>
-           
-            <div className='container-double-text'>
-              <div>I help companies from all over the world with tailor made solutions. With each project, I push my work to new horizons, always putting quality first.</div>
-              <div className='secondtext-short'>Always Exploring</div>
-            </div>
-        </div>
+          <div className='first-title'>Helping brands thrive</div>
+          <div className='first-title'>in the digital world</div>
+          <div className='line'>
+            <div className='circle-overline'></div>
+          </div>
 
         </div>
 
-        <div className='container-image-about'>
-        <img src={victorPic} alt="Victor in Albania" />
-        </div>
+        <div className='container-second'>
 
-      </div>
-
-      <div className='container-third'>
-        <div className='third-title'> I can help you with...</div>
-
-        <div className='container-options'>
-
-          <div className='boxes-about'>
-            <h2>Design</h2>
-            <p>
-              With a solid track record in designing websites, I deliver strong and user-friendly digital designs. (Since 2024 only in combination with development)
-            </p>
-
+          <div className='container-textfirst-about'>
+          <div className='container-flechita-text'>
             
+              <div className='container-double-text'>
+                <div>I help companies from all over the world with tailor made solutions. With each project, I push my work to new horizons, always putting quality first.</div>
+                <div className='secondtext-short'>Always Exploring</div>
+              </div>
           </div>
-          <div className='boxes-about'>
-          <h2>Development</h2>
-            <p>
-            I build scalable websites from scratch that fit seamlessly with design. My focus is on micro animations, transitions and interaction. Build with React, Vanilla or any JavaScript framework.
-            </p>
+
           </div>
-          <div className='boxes-about'>
-          <h2>The full package</h2>
-            <p>
-            A complete website from concept to implementation, that's what makes me stand out. My great sense for design and my development skills enable me to create kick-ass projects.
-            </p>
+
+          <div className='container-image-about'>
+          <img src={victorPic} alt="Victor in Albania" />
           </div>
 
         </div>
 
+        <div className='container-third'>
+          <div className='third-title'> I can help you with...</div>
+
+          <div className='container-options'>
+
+            <div className='boxes-about'>
+              <h2>Design</h2>
+              <p>
+                With a solid track record in designing websites, I deliver strong and user-friendly digital designs. (Since 2024 only in combination with development)
+              </p>
+
+              
+            </div>
+            <div className='boxes-about'>
+            <h2>Development</h2>
+              <p>
+              I build scalable websites from scratch that fit seamlessly with design. My focus is on micro animations, transitions and interaction. Build with React, Vanilla or any JavaScript framework.
+              </p>
+            </div>
+            <div className='boxes-about'>
+            <h2>The full package</h2>
+              <p>
+              A complete website from concept to implementation, that's what makes me stand out. My great sense for design and my development skills enable me to create kick-ass projects.
+              </p>
+            </div>
+
+          </div>
+
+        </div>
+        <Footer />
+
       </div>
-      <Footer />
     
       </>
     )
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,116 +1,128 @@
-import React from "react";
+import React, {useEffect, useRef} from "react";
+import gsap from 'gsap';
+
 import './Contact.css';
 import Header from '../Header/Header'
 
 
 const Contact = () => {
+  const contactRef = useRef(null);
+
+
+    // Immediately set opacity to 1 once component mounts
+    useEffect(() => {
+      gsap.set(contactRef.current, { opacity: 1 });
+    }, []);
 
   return (
     <>
-              <Header />
-              <div className="contact-container">
 
-                <div className="contact-container-title">
+      <div className="contact-main-container" ref={contactRef}>
 
-               
-                  <div className="contact-img-title-container">
-                    <img className="contact-img"></img>
-                    <h2>Let's start a </h2>
-                  </div>
-                  <h2>project together</h2>
+          <Header />
+          <div className="contact-container">
 
+            <div className="contact-container-title">
+
+            
+              <div className="contact-img-title-container">
+                <img className="contact-img"></img>
+                <h2>Let's start a </h2>
+              </div>
+              <h2>project together</h2>
+
+            </div>
+            <section>
+        <svg
+          className="flecha-icon"
+          width="15"
+          height="15"
+          viewBox="0 0 52 52"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect x="13" y="45" width="38" height="6" fill="black" />
+          <rect
+            x="51"
+            y="13"
+            width="38"
+            height="6"
+            transform="rotate(90 51 13)"
+            fill="black"
+          />
+          <rect
+            x="4.67915"
+            y="0.436523"
+            width="65.6669"
+            height="6"
+            transform="rotate(45 4.67915 0.436523)"
+            fill="black"
+          />
+        </svg>
+    
+              </section>
+
+
+          <div className="contact-form-details-container">
+
+            <div className="contact-container-form">
+
+              <form class="contact-minimal-form">
+                <div class="contact-form-group">
+                  <label for="name">What's your name?</label>
+                  <input type="text" id="name" name="name" placeholder="John Doe *" required></input>
                 </div>
-                <section>
-            <svg
-              className="flecha-icon"
-              width="15"
-              height="15"
-              viewBox="0 0 52 52"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <rect x="13" y="45" width="38" height="6" fill="black" />
-              <rect
-                x="51"
-                y="13"
-                width="38"
-                height="6"
-                transform="rotate(90 51 13)"
-                fill="black"
-              />
-              <rect
-                x="4.67915"
-                y="0.436523"
-                width="65.6669"
-                height="6"
-                transform="rotate(45 4.67915 0.436523)"
-                fill="black"
-              />
-            </svg>
-        
-                  </section>
-
-
-              <div className="contact-form-details-container">
-
-                <div className="contact-container-form">
-
-                  <form class="contact-minimal-form">
-                    <div class="contact-form-group">
-                      <label for="name">What's your name?</label>
-                      <input type="text" id="name" name="name" placeholder="John Doe *" required></input>
-                    </div>
-                    <div class="contact-form-group">
-                      <label for="email">What's your email?</label>
-                      <input type="email" id="email" name="email" placeholder="john@doe.com*" required/>
-                    </div>
-                    <div class="contact-form-group">
-                      <label for="organization">What's the name of your organization?</label>
-                      <input type="text" id="organization" name="organization" placeholder="John & Doe ®"/>
-                    </div>
-                    <div class="contact-form-group">
-                      <label for="services">What services are you looking for?</label>
-                      <input type="text" id="services" name="services" placeholder="Web Design, Web Development ..."/>
-                    </div>
-                    <div class="contact-form-group">
-                      <label for="message">Your message</label>
-                      <textarea id="message" name="message" placeholder="Hello Victor, can you help me with ...*" required rows="6" cols="50"></textarea>
-                    </div>
-                    <button type="submit" class="contact-submit-button">Submit</button>
-                  </form>
-
+                <div class="contact-form-group">
+                  <label for="email">What's your email?</label>
+                  <input type="email" id="email" name="email" placeholder="john@doe.com*" required/>
                 </div>
-
-
-                <div className="contact-container-details">
-
-                  <div className="contact-detail-container">
-                    <div className="detail-title">CONTACT DETAILS</div>
-                    <div className="detail-content">victormaragioglio1228@gmail.com</div>
-                    <div className="detail-content">+49 1523 6786964</div>
-                  </div>
-
-                  <div  className="contact-detail-container">
-                    <div className="detail-title">BUSINESS DETAILS</div>
-                    <div className="detail-content">Victor Maragioglio</div>
-                    <div className="detail-content">Location: South Wales</div>
-                  </div>
-
-                  <div  className="contact-detail-container">
-                    <div className="detail-title">SOCIALS</div>
-                    <div className="detail-content">Instagram</div>
-                    <div className="detail-content">Behance</div>
-                    <div className="detail-content">LinkedIn</div>
-                  </div>
-
+                <div class="contact-form-group">
+                  <label for="organization">What's the name of your organization?</label>
+                  <input type="text" id="organization" name="organization" placeholder="John & Doe ®"/>
                 </div>
+                <div class="contact-form-group">
+                  <label for="services">What services are you looking for?</label>
+                  <input type="text" id="services" name="services" placeholder="Web Design, Web Development ..."/>
+                </div>
+                <div class="contact-form-group">
+                  <label for="message">Your message</label>
+                  <textarea id="message" name="message" placeholder="Hello Victor, can you help me with ...*" required rows="6" cols="50"></textarea>
+                </div>
+                <button type="submit" class="contact-submit-button">Submit</button>
+              </form>
 
+            </div>
+
+
+            <div className="contact-container-details">
+
+              <div className="contact-detail-container">
+                <div className="detail-title">CONTACT DETAILS</div>
+                <div className="detail-content">victormaragioglio1228@gmail.com</div>
+                <div className="detail-content">+49 1523 6786964</div>
               </div>
 
-
+              <div  className="contact-detail-container">
+                <div className="detail-title">BUSINESS DETAILS</div>
+                <div className="detail-content">Victor Maragioglio</div>
+                <div className="detail-content">Location: South Wales</div>
               </div>
 
-      
+              <div  className="contact-detail-container">
+                <div className="detail-title">SOCIALS</div>
+                <div className="detail-content">Instagram</div>
+                <div className="detail-content">Behance</div>
+                <div className="detail-content">LinkedIn</div>
+              </div>
+
+            </div>
+
+          </div>
+
+
+          </div>
+
+      </div>
 
     </>
   );
