4df528ff18c99389ea67b5da3176183d6a6d284c
maragiogliov <maragiogliov@protonmail.com>
2024-07-19 23:30:20 +0200

    go

--- src/components/Landing_02/Landing_02.js
+++ src/components/Landing_02/Landing_02.js
@@ -1,24 +1,50 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Landing_02.css';
+import thebarn from '../../assets/3-devices-black.png';
+import imageSrc from '../../assets/Frame 32.jpg';
+import cvDesign from '../../assets/xvcwebsite.png';
+import mothercode from '../../assets/mothercode.png';
+import greenhousecalculator from '../../assets/greenhousecalculator.png';
+import vportfolio from '../../assets/v-portfolio.png';
 
+export default function Landing_02() {
+  const [projects, setProjects] = useState([]);
 
-
-
-
-
-export default function About() {
-
-
+  useEffect(() => {
+    fetch('/WorksData.json')
+      .then(response => response.json())
+      .then(data => {
+        const updatedData = data.map(project => {
+          if (project.image === 'thebarn') project.image = thebarn;
+          if (project.image === 'imageSrc') project.image = imageSrc;
+          if (project.image === 'cvDesign') project.image = cvDesign;
+          if (project.image === 'mothercode') project.image = mothercode;
+          if (project.image === 'greenhousecalculator') project.image = greenhousecalculator;
+          if (project.image === 'vportfolio') project.image = vportfolio;
+          return project;
+        });
+        setProjects(updatedData);
+      })
+      .catch(error => console.error('Error fetching the projects data:', error));
+  }, []);
 
   return (
-      <>
+    <>
       <div className='landing-02-main-container'>
-
-      
-      <div className='title-landing-02'>RECENT PROJECTS</div>
-
+        <div className='title-landing-02'>RECENT PROJECTSS</div>
+        <div className='projects-container'>
+          {projects.slice(0, 3).map((project, index) => (
+            <div className='project-card' key={index}>
+              <div className='project-info'>
+                <h2>{project.name}</h2>
+                <p>{project.location}</p>
+                <p>{project.services}</p>
+                <p>{project.year}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-    
-      </>
-    )
-  }
+    </>
+  );
+}
--- src/components/Work/Work.js
+++ src/components/Work/Work.js
@@ -2,123 +2,42 @@ import React, { useState, useEffect, useRef } from 'react';
 import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import './Work.css';
+import thebarn from '../../assets/3-devices-black.png';
 import imageSrc from '../../assets/Frame 32.jpg';
 import cvDesign from '../../assets/xvcwebsite.png';
 import mothercode from '../../assets/mothercode.png';
 import greenhousecalculator from '../../assets/greenhousecalculator.png';
 import vportfolio from '../../assets/v-portfolio.png';
-import thebarn from '../../assets/3-devices-black.png';
-import Header from '../Header/Header'
+import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 gsap.registerPlugin(ScrollTrigger);
 
-const projects = [
-  {
-    name: 'The Barn',
-    location: 'Germany',
-    services: 'Design & Development',
-    year: '2023',
-    description: 'Transformed Shopify architecture, improved UX by app management. Enhanced products with metafields, extracted insights from reports. Crafted responsive interfaces, collaborated cross-functionally at The Barn Coffee Roasters.',
-    image: thebarn,
-    technologies: ['Shopify', 'UI'],
-    buttons: [
-      {
-        text: 'Web',
-        url: 'https://thebarn.de/'
-      },
-    ]
-  },
-  {
-    name: 'V.PORTFOLIO',
-    location: 'Germany',
-    services: 'Design & Development',
-    year: '2024',
-    description: 'This is my personal portfolio where you can check my latest work and more about my journey and who I am.',
-    image: vportfolio,
-    technologies: ['React', 'UI'],
-    buttons: [
-      {
-        text: 'Web',
-        url: 'https://vm-portfolio-iota.vercel.app/'
-      },
-      {
-        text: 'Github',
-        url: 'https://github.com/maragiogliov/vm-portfolio'
-      }
-    ]
-  },
-  {
-    name: 'GH Calculator',
-    location: 'Denmark',
-    services: 'Design & Development',
-    year: '2023',
-    description: 'Designed and developed a B2C carbon footprint calculator using React.js, Redux, and IBM Carbon Design System.',
-    image: greenhousecalculator,
-    technologies: ['React', 'UI'],
-    buttons: [
-      {
-        text: 'Web',
-        url: 'https://www.greenhousecalculator.com'
-      }
-    ]
-  },
-  {
-    name: 'Lenguapp',
-    location: 'Germany',
-    services: 'Design',
-    year: '2022',
-    description: 'I conducted a UX research study for a language learning app, including competitive analysis, user interviews, personas, wireframing, prototyping, and usability tests.',
-    image: imageSrc,
-    technologies: ['UX Research'],
-    buttons: [
-      {
-        text: 'Behance',
-        url: 'https://www.behance.net/vmaragioglio'
-      }
-    ]
-  },
-  {
-    name: 'CV Desserts',
-    location: 'USA',
-    services: 'Design',
-    year: '2022',
-    description: 'I designed a visually appealing dessert website for a Florida client, highlighting ease of use, appetizing photos, simple ordering, and transparent values. A delight for eyes and taste buds!',
-    image: cvDesign,
-    technologies: ['Shopify', 'UI'],
-    buttons: [
-      {
-        text: 'Behance',
-        url: 'https://www.behance.net/gallery/139554081/Web-Design-for-CV-Desserts'
-      }
-    ]
-  },
-  {
-    name: 'MOTHERCODE',
-    location: 'Germany',
-    services: 'Design & Development',
-    year: '2021',
-    description: 'Our Educational Management App simplifies learning for DCI Berlin School students, enabling code visualization, learning tracking, and exercise management.',
-    image: mothercode,
-    technologies: ['React', 'Express', 'Full Stack'],
-    buttons: [
-      {
-        text: 'Youtube',
-        url: 'https://www.youtube.com/watch?v=PtlyAgL-d38&t=7s'
-      },
-      {
-        text: 'GitHub',
-        url: 'https://github.com/maragiogliov/mothercode-front-end'
-      }
-    ]
-  }
-];
-
 export default function Work() {
   const [filter, setFilter] = useState('All');
-  const [filteredProjects, setFilteredProjects] = useState(projects);
+  const [projects, setProjects] = useState([]);
+  const [filteredProjects, setFilteredProjects] = useState([]);
   const workRef = useRef(null);
 
+  useEffect(() => {
+    fetch('/WorksData.json')
+      .then(response => response.json())
+      .then(data => {
+        // Replace placeholders with actual image imports
+        const updatedData = data.map(project => {
+          if (project.image === 'thebarn') project.image = thebarn;
+          if (project.image === 'imageSrc') project.image = imageSrc;
+          if (project.image === 'cvDesign') project.image = cvDesign;
+          if (project.image === 'mothercode') project.image = mothercode;
+          if (project.image === 'greenhousecalculator') project.image = greenhousecalculator;
+          if (project.image === 'vportfolio') project.image = vportfolio;
+          return project;
+        });
+        setProjects(updatedData);
+        setFilteredProjects(updatedData);
+      })
+      .catch(error => console.error('Error fetching the projects data:', error));
+  }, []);
 
   useEffect(() => {
     let filtered = projects;
@@ -128,88 +47,64 @@ export default function Work() {
       filtered = projects.filter(project => project.services.includes('Development'));
     }
     setFilteredProjects(filtered);
-  }, [filter]);
+  }, [filter, projects]);
 
-    // Immediately set opacity to 1 once component mounts
-    useEffect(() => {
-      gsap.set(workRef.current, { opacity: 1 });
-    }, []);
+  useEffect(() => {
+    gsap.set(workRef.current, { opacity: 1 });
+  }, []);
 
   return (
-    <>
     <div className='work-main-container' ref={workRef}>
+      <Header />
+      <div className='work-title-container'>
+        <h2>Creating next level</h2>
+        <h2>digital products</h2>
+      </div>
 
-        <Header />
-        <div className='work-title-container'>
-          <h2>Creating next level</h2>
-          <h2>digital products</h2>
+      <div className='work-filter-container'>
+        <div className='work-filters'>
+          <button className={filter === 'All' ? 'active' : ''} onClick={() => setFilter('All')}>All</button>
+          <button className={filter === 'Design' ? 'active' : ''} onClick={() => setFilter('Design')}>Design</button>
+          <button className={filter === 'Development' ? 'active' : ''} onClick={() => setFilter('Development')}>Development</button>
         </div>
+        <div className='work-filter-view'>
+          <button>List</button>
+          <button>Square</button>
+        </div>
+      </div>
 
-        <div className='work-filter-container'>
-          <div className='work-filters'>
-            <button 
-              className={filter === 'All' ? 'active' : ''} 
-              onClick={() => setFilter('All')}
-            >
-              All
-            </button>
-            <button 
-              className={filter === 'Design' ? 'active' : ''} 
-              onClick={() => setFilter('Design')}
-            >
-              Design
-            </button>
-            <button 
-              className={filter === 'Development' ? 'active' : ''} 
-              onClick={() => setFilter('Development')}
-            >
-              Development
-            </button>
-          </div>
-
-          <div className='work-filter-view'>
-            <button>List</button>
-            <button>square</button>
+      <div className='container-mvp'>
+        <div className='description-row-container'>
+          <div className='description-client-container'>CLIENT</div>
+          <div className='description-location-container'>LOCATION</div>
+          <div className='description-servi-year-container'>
+            <div className='description-services-container'>SERVICES</div>
+            <div className='description-year-container'>YEAR</div>
           </div>
         </div>
 
-        <div className='container-mvp'>
-          <div className='description-row-container'>
-            <div className='description-client-container'>CLIENT</div>
-            <div className='description-location-container'>LOCATION</div>
-            <div className='description-servi-year-container'>
-              <div className='description-services-container'>SERVICES</div>
-              <div className='description-year-container'>YEAR</div>
-            </div>
-          </div>
-
-          {filteredProjects.map((project, index) => (
-            <div className='section-mvp card' key={index}>
-              <div className='card-text-container'>
-                <div className='projects-title'>
-                  <h2>{project.name}</h2>
+        {filteredProjects.map((project, index) => (
+          <div className='section-mvp card' key={index}>
+            <div className='card-text-container'>
+              <div className='projects-title'>
+                <h2>{project.name}</h2>
+              </div>
+              <div className='projects-location'>
+                <h2>{project.location}</h2>
+              </div>
+              <div className='servi-year-container'>
+                <div className='projects-services'>
+                  <h2>{project.services}</h2>
                 </div>
-
-                <div className='projects-location'>    
-                  <h2>{project.location}</h2>
-                </div>
-
-                <div className='servi-year-container'>
-                  <div className='projects-services'>    
-                    <h2>{project.services}</h2>
-                  </div>
-
-                  <div className='projects-year'>    
-                    <h2>{project.year}</h2>
-                  </div>
+                <div className='projects-year'>
+                  <h2>{project.year}</h2>
                 </div>
               </div>
             </div>
-          ))}
-        </div>
-        <Footer />
-
+          </div>
+        ))}
+      </div>
+      <Footer />
     </div>
-    </>
   );
 }
