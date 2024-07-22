1607387083df695161de71694adcfcd37b5d1f31
maragiogliov <maragiogliov@protonmail.com>
2024-07-22 19:44:57 +0200

    go

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,65 +1,65 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Footer.css';
 
 export default function Footer() {
+  const [localTime, setLocalTime] = useState('');
+
+  useEffect(() => {
+    const updateLocalTime = () => {
+      const date = new Date();
+      const options = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+      setLocalTime(date.toLocaleTimeString([], options));
+    };
+
+    // Initial time set
+    updateLocalTime();
+
+    // Update time every second
+    const intervalId = setInterval(updateLocalTime, 1000);
+
+    // Cleanup interval on component unmount
+    return () => clearInterval(intervalId);
+  }, []);
+
   return (
     <div className="footer-container">
-
-
       <div className='footer-first-container'>
-
         <div className='headshot-fulltitle-container'>
-
-            <div className='headshot-title-container'>
-
-                <img className='footer-headshot'/>
-                <div className='footer-title'>Let's work </div>
-
-            </div>
-
-            <div className='footer-title'>together</div>
-
+          <div className='headshot-title-container'>
+            <img className='footer-headshot' alt="Headshot" />
+            <div className='footer-title'>Let's work </div>
+          </div>
+          <div className='footer-title'>together</div>
         </div>
-
         <div className='line'>
-            <a href='/contact' className='circle-overline'>Get in touch</a>
+          <a href='/contact' className='circle-overline'>Get in touch</a>
         </div>
-
         <div className='buttons-footer-container'>
-          <button> victormaragioglio@gmail.com</button>
-          <button> +49 1523 6786964</button>
+          <button>victormaragioglio@gmail.com</button>
+          <button>+49 1523 6786964</button>
         </div>
-
       </div>
-      
-
 
       <div className='footer-bottom-info-container'>
-
         <div className='footer-version-time-container'>
           <div className='footer-version-container'>
             <div>VERSION</div>
-            <div>2024©Edtion</div>
+            <div>2024©Edition</div>
           </div>
-
           <div>
             <div>LOCAL TIME</div>
-            <div>03:16PM GMT+2</div>
+            <div>{localTime}</div>
           </div>
         </div>
-
         <div>
           <div>SOCIALS</div>
           <div className='footer-social-container'>
-            <a>Instagram</a>
-            <a>Behance</a>
-            <a>LinkedIn</a>
+            <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">Instagram</a>
+            <a href="https://behance.net" target="_blank" rel="noopener noreferrer">Behance</a>
+            <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer">LinkedIn</a>
           </div>
         </div>
-
       </div>
-    
     </div>
   );
 }
-
